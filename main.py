d9716cb6e2c436e7371291bc10bcbeec9b0ad77a
Alice Vinigradova <zazaliska@gmail.com>
2022-12-18 00:49:56 +0300

    по идее полностью рабочая версия

--- main.py
+++ main.py
@@ -47,6 +47,11 @@ def start_welcome(message):
 
 
 def user_registration(message):
+    if user_in_db(message.from_user.id):
+        bot.send_message(message.chat.id,
+                         "Ты уже зарегистрирован в нашей системе. Пока это можно сделать только один раз.")
+        return
+
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     itembtn1 = types.KeyboardButton("абитуриент")
     itembtn2 = types.KeyboardButton("студент")
@@ -58,94 +63,99 @@ def user_registration(message):
 
 @bot.message_handler(content_types='text')
 def text_message(message):
-    print(message.text)
-    print(users)
-    # global student_fac
-    # global recommends, cur, fac, br, rec_brs
-    if message.text == 'абитуриент':
-        user = User(message.text)
-        users[message.chat.id] = user
+    try:
+        # # print('message.text', message.text)
+        # print('users', users)
+        # global student_fac
+        # global recommends, cur, fac, br, rec_brs
+        if message.text == 'абитуриент':
+            user = User(message.text)
+            users[message.chat.id] = user
 
-        abiturient_registration(message)
-    elif message.text == 'студент':
-        user = User(message.text)
-        users[message.chat.id] = user
+            abiturient_registration(message)
+        elif message.text == 'студент':
+            user = User(message.text)
+            users[message.chat.id] = user
 
-        student_registration(message)
-    elif message.text in get_all_spheres():
-        print('Before user_in_db ', message.from_user.id)
-        if not user_in_db(message.from_user.id):
-            put_abitur_into_db(message.from_user.id, message.text)
+            student_registration(message)
+        elif message.text in get_all_spheres():
+            # print('Before user_in_db ', message.from_user.id)
+            if not user_in_db(message.from_user.id):
+                put_abitur_into_db(message.from_user.id, message.text)
 
-        bot.send_message(message.chat.id,
-                         'Поздравляю! Ты закончил вводную часть. Теперь просто отмечай понравившиеся факультеты и '
-                         'направления.\nСтуденты свяжутся с тобой!')
-
-        # abiturient_recomendations(message)
-
-        telebot.types.ReplyKeyboardRemove()
-
-        users[message.chat.id].recommends = get_recomendations(message.from_user.id)
-        print(users[message.chat.id].recommends)
-        send_recommendations(message)
-        users[message.chat.id].cur = 'fac'
-        users[message.chat.id].fac = 0
-        users[message.chat.id].br = 0
-    elif message.text in get_all_faculties():
-        users[message.chat.id].student_fac = message.text
-        student_registration_finish(message)
-    elif message.text in get_branches(users[message.chat.id].student_fac):
-        student_branch = message.text
-
-        bot.send_message(message.chat.id,
-                         'Поздравляю! Ты закончил вводную часть. Теперь жди абитуриентов, нуждающихся в твоей помощи.')
-
-        if not user_in_db(message.from_user.id):
-            put_student_into_db(message.from_user.id, users[message.chat.id].student_fac, student_branch)
-
-    elif message.text == 'Интересует':
-        if users[message.chat.id].cur == 'fac':
-            users[message.chat.id].cur = 'br'
-            users[message.chat.id].rec_brs = get_rec_branches(users[message.chat.id].recommends[users[message.chat.id].fac])
-            print(users[message.chat.id].rec_brs)
-            send_recommendations(message)
-        elif users[message.chat.id].cur == 'br':
             bot.send_message(message.chat.id,
-                             'Ваша заявка отправлена студентам.\nТеперь дождитесь ответа')
-            send_student_request(message)
-            if users[message.chat.id].br + 1 == len(users[message.chat.id].rec_brs):
+                             'Поздравляю! Ты закончил вводную часть. Теперь просто отмечай понравившиеся факультеты и '
+                             'направления.\nСтуденты свяжутся с тобой!')
+
+            # abiturient_recomendations(message)
+
+            telebot.types.ReplyKeyboardRemove()
+
+            users[message.chat.id].recommends = get_recomendations(message.from_user.id)
+            # print('recommends', users[message.chat.id].recommends)
+            send_recommendations(message)
+            users[message.chat.id].cur = 'fac'
+            users[message.chat.id].fac = 0
+            users[message.chat.id].br = 0
+        elif message.text in get_all_faculties():
+            users[message.chat.id].student_fac = message.text
+            student_registration_finish(message)
+        elif message.text in get_branches(users[message.chat.id].student_fac):
+            student_branch = message.text
+
+            bot.send_message(message.chat.id,
+                             'Поздравляю! Ты закончил вводную часть. Теперь жди абитуриентов, нуждающихся в твоей помощи.')
+
+            if not user_in_db(message.from_user.id):
+                put_student_into_db(message.from_user.id, users[message.chat.id].student_fac, student_branch)
+
+        elif message.text == 'Интересует':
+            if users[message.chat.id].cur == 'fac':
+                users[message.chat.id].cur = 'br'
+                users[message.chat.id].rec_brs = get_rec_branches(users[message.chat.id].recommends[users[message.chat.id].fac])
+                # print('rec_brs', users[message.chat.id].rec_brs)
+                send_recommendations(message)
+            elif users[message.chat.id].cur == 'br':
+                bot.send_message(message.chat.id,
+                                 'Ваша заявка отправлена студентам.\nТеперь дождитесь ответа')
+                send_student_request(message)
+                if users[message.chat.id].br + 1 == len(users[message.chat.id].rec_brs):
+                    if users[message.chat.id].fac + 1 == len(users[message.chat.id].recommends):
+                        bot.send_message(message.from_user.id, 'На этом пока все! Спасибо за участие')
+                        del users[message.chat.id]
+                        return
+                    users[message.chat.id].cur = 'fac'
+                    users[message.chat.id].fac += 1
+                    users[message.chat.id].br = 0
+                else:
+                    users[message.chat.id].br += 1
+                send_recommendations(message)
+
+        elif message.text == 'Не интересует':
+            if users[message.chat.id].cur == 'fac':
                 if users[message.chat.id].fac + 1 == len(users[message.chat.id].recommends):
                     bot.send_message(message.from_user.id, 'На этом пока все! Спасибо за участие')
                     del users[message.chat.id]
                     return
-                users[message.chat.id].cur = 'fac'
                 users[message.chat.id].fac += 1
-                users[message.chat.id].br = 0
-            else:
-                users[message.chat.id].br += 1
-            send_recommendations(message)
+                send_recommendations(message)
+            elif users[message.chat.id].cur == 'br':
+                if users[message.chat.id].br + 1 == len(users[message.chat.id].rec_brs):
+                    users[message.chat.id].cur = 'fac'
+                    users[message.chat.id].fac += 1
+                    users[message.chat.id].br = 0
+                else:
+                    users[message.chat.id].br += 1
+                send_recommendations(message)
 
-    elif message.text == 'Не интересует':
-        if users[message.chat.id].cur == 'fac':
-            if users[message.chat.id].fac + 1 == len(users[message.chat.id].recommends):
-                bot.send_message(message.from_user.id, 'На этом пока все! Спасибо за участие')
-                del users[message.chat.id]
-                return
-            users[message.chat.id].fac += 1
-            send_recommendations(message)
-        elif users[message.chat.id].cur == 'br':
-            if users[message.chat.id].br + 1 == len(users[message.chat.id].rec_brs):
-                users[message.chat.id].cur = 'fac'
-                users[message.chat.id].fac += 1
-                users[message.chat.id].br = 0
-            else:
-                users[message.chat.id].br += 1
-            send_recommendations(message)
+    except Exception as e:
+        # print(repr(e))
+        bot.send_message(message.from_user.id, 'На этом пока все! Спасибо за участие')
 
 
 def abiturient_registration(message):
     all_spheres = get_all_spheres()
-    print(all_spheres)
+    # print('all_spheres', all_spheres)
     reply_keyboard = []
     for i in range(len(all_spheres[::2])):
         reply_keyboard.append([all_spheres[i], all_spheres[i + 3]])
@@ -163,7 +173,7 @@ def abiturient_registration(message):
 
 def student_registration(message):
     all_faculties = get_all_faculties()
-    print(all_faculties)
+    # print('all_faculties', all_faculties)
     reply_keyboard = []
     for i in range(len(all_faculties[::2])):
         reply_keyboard.append([all_faculties[i], all_faculties[i + 5]])
@@ -181,7 +191,7 @@ def student_registration(message):
 def student_registration_finish(message):
     # global student_fac
     all_branches = get_branches(users[message.chat.id].student_fac)
-    print(all_branches)
+    # print('all_branches', all_branches)
     reply_keyboard = []
     for i in range(len(all_branches[:len(all_branches)-1:2])):
         reply_keyboard.append([all_branches[i], all_branches[i + len(all_branches) // 2]])
@@ -217,10 +227,10 @@ def send_recommendations(message):
 
 def send_student_request(message):
     students = get_students(users[message.chat.id].rec_brs[users[message.chat.id].br])
-    print(students)
+    # print('students', students)
     if message.from_user.username and students:
         for id in students:
-            print(id)
+            # print(id)
             bot.send_message(id, f'Абитуриент https://t.me/{message.from_user.username} хочет получить консультацию. '
                                  f'Напиши ему и ответь на интересующие вопросы!')
 
